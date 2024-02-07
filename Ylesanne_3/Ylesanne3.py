db527c7f73ed687aaccbc0b0ac84203a500baa88
raivoarras1 <raivo.arras@vikk.ee>
2024-02-07 09:10:05 +0200

    Tarvara_arendus

--- Ylesanne_3/Ylesanne3.py
+++ Ylesanne_3/Ylesanne3.py
@@ -1,15 +1,20 @@
 import pygame
 
-def ruudustik(screen, mode, cell_size, color):      #funktsioon ruudustiku joonistamiseks
-    i = 0                                           #ruudustiku algkoordinaat x
-    j = 0                                           #ruudustiku algkoordinaat y
-    while j < mode[1]:                              #tsükkel seni kuni j on väiksem ekraani mõõtmetest
-        while i < mode[0]:                          #tsükkel seni kuni i on väiksem ekraani mõõtmetest
-            #joonista ruut koordinaadiga (i, j) ja suurusega cell_size
+def ruudustik(screen, mode, cell_size, color, rida, veerg):      #funktsioon ruudustiku joonistamiseks
+    i = 0                                       #ruudustiku algkoordinaat x
+    j = 0                                       #ruudustiku algkoordinaat y
+    r = 0                                       #rea numbri algväärtus
+    v = 0                                       #veeru numbri algväärtus
+    while j < mode[1] and r < rida:             #tsükkel seni kuni j on väiksem ekraani mõõtmetest ja ridade arv on väiksem etteantud arvust
+        while i < mode[0] and v < veerg:        #tsükkel seni kuni i on väiksem ekraani mõõtmetest ja veergude arv on väiksem etteantud arvust
+            #joonista ruut koordinaadiga (i, j) ja suurusega "cell_size" * "cell_zize"
             pygame.draw.rect(screen, color, [i, j, cell_size, cell_size], 1)
-            i += cell_size                          #liigu cell_size võrra paremale
-        j += cell_size                              #kui rida on täis, siis liigu cell_size võrra alla
-        i = 0                                       #liigu tagasi rea algusesse
+            i += cell_size                      #liigu "cell_size" võrra paremale
+            v += 1                              #veeru numbri suurendamine
+        j += cell_size                          #kui rida on täis, siis liigu cell_size võrra alla
+        r += 1                                  #rea numbri suurendamine
+        i = 0                                   #liigu tagasi rea algusesse
+        v = 0                                   #veeru numbri "reset"
 
 pygame.init()
 
@@ -20,21 +25,23 @@ blue = [0, 0, 255]
 yellow = [255, 255, 0]
 
 #Ekraani parameetrid
-mode = [640, 480]                                       #akna suurus, mida kasutada
-pygame.display.set_caption("Ülesanne3 - Raivo Arras")   #akna pealkiri
-screen = pygame.display.set_mode(mode)                  #akna loomine
-screen.fill([153, 255, 153])                            #akna taustavärv
+mode = [640, 480]                                           #akna suurus, mida kasutada
+pygame.display.set_caption("Ülesanne3 - Raivo Arras")       #akna pealkiri
+screen = pygame.display.set_mode(mode)                      #akna loomine
+screen.fill([153, 255, 153])                                #akna taustavärv
 
-cell_size = 20                                          #ruudu suuruse valimine
-color = red                                             #joone värvi valimine
-ruudustik(screen, mode, cell_size, color)               #ruudustiku joonistamine
+cell_size = 20                                              #ruudu suuruse valimine
+color = red                                                 #joone värvi valimine
+rida = 100                                                  #mitu rida joonistada (seni kuni mahub)
+veerg = 100                                                 #mitu veergu joonistada (seni kuni mahub)
+ruudustik(screen, mode, cell_size, color, rida, veerg)      #ruudustiku joonistamine
 
-pygame.display.flip()                                   #ekraanipildi värskendamine
+pygame.display.flip()                                       #ekraanipildi värskendamine
 running = True
 
-while running:                                          #ekraani lahti hoidmine
+while running:                                              #ekraani lahti hoidmine
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-pygame.quit()                                           #ekraani sulgemine
+pygame.quit()                                               #ekraani sulgemine
